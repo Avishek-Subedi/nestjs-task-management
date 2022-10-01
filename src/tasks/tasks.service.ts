011f8b92b91298c7fbcfc0e464b3ad69d5a8641c
Avishek <abishek.subedi@spiralogics.com>
2022-10-01 20:56:25 +0545

    uuid imported

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid';
-
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -12,10 +11,10 @@ export class TasksService {
 
   createTask(title: string, description: string): Task {
     const task: Task = {
+      id: uuidv4(),
       title,
       description,
       status: TaskStatus.OPEN,
-      id: uuid(),
     };
 
     this.tasks.push(task);
